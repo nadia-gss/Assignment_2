bb7ae15a987d966dd96fd5b561fdaafb70388377
nadia-gss <151856390+nadia-gss@users.noreply.github.com>
2024-04-14 15:26:22 +0100

    Update control_gpio4.c

--- Assignment2/control_gpio4.c
+++ Assignment2/control_gpio4.c
@@ -1,7 +1,7 @@
 #include <pigpio.h>
 #include <stdio.h>
 
-#define PIN 4  // GPIO4 对应的BCM引脚编号
+#define PIN 4 
 
 int main() {
     if (gpioInitialise() < 0) {
@@ -9,19 +9,15 @@ int main() {
         return 1;
     }
 
-    // 设置 PIN 为输出
     gpioSetMode(PIN, PI_OUTPUT);
 
-    // 拉高 GPIO4
     gpioWrite(PIN, 1);
     printf("GPIO4 set to HIGH\n");
-    time_sleep(5); // 等待5秒
+    time_sleep(5);
 
-    // 拉低 GPIO4
     gpioWrite(PIN, 0);
     printf("GPIO4 set to LOW\n");
 
-    // 清理并退出
     gpioTerminate();
     return 0;
 }
